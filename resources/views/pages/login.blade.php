b53dccdfc9e0d4ad2111e64e88761d957bf959f0
Kenneth T. Otsuka <otsukakenneth@yahoo.com>
2015-06-30 14:55:41 +0800

    commit

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -18,8 +18,8 @@
 	<link href="/css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- Custom CSS -->
-	<link href="/css/sb-admin.css" rel="stylesheet">
-
+	<link href="/css/signin.css" rel="stylesheet">
+	
 	<!-- Morris Charts CSS -->
 	<link href="/css/plugins/morris.css" rel="stylesheet">
 
@@ -29,6 +29,28 @@
 
 
 <body>
+	
+	<div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 
 </html>
