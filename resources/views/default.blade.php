e7b8ccc79e2d14386dcf0af6d887d8dca38e2644
roannadavid <roannadavid@yahoo.com>
2015-07-01 09:40:45 +0800

    Added sidebar options for super admin

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -90,12 +90,13 @@
     </div><!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Own javascript -->
-    <script src="js/app.js"></script>
+    <script src="/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+
+    <!-- theme js -->
+    <script src="/js/app.min.js" type="text/javascript"></script>
+
   </body>
 </html>
--- resources/views/superadmin/index.blade.php
+++ resources/views/superadmin/index.blade.php
@@ -21,8 +21,17 @@ Transaction List
   <li>
       <a href="#"><i class="fa fa-fw fa-exchange"></i>Assign Transactions</a>
   </li>
-  <li>
-      <a href="#"><i class="fa fa-fw fa-group"></i>Manage Users</a>
+  <li class="treeview">
+    <a href="#">
+      <i class="fa fa-group"></i>
+      <span>Manage Users</span>
+      <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+      <li><a href="#"><i class="fa fa-list"></i> View Users</a></li>
+      <li><a href="#"><i class="fa fa-user"></i> Set Administrators</a></li>
+      <li><a href="#"><i class="fa fa-plus-circle"></i> Add User</a></li>
+    </ul>
   </li>
 @stop
 
