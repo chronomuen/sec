13995754e3ad1ec62a8f15e23c6332f716b78867
roannadavid <roannadavid@yahoo.com>
2015-07-01 13:09:15 +0800

    Created transaction creation page

---
+++ resources/views/forms/_new_transaction.blade.php
@@ -0,0 +1,36 @@
+<!-- Form input fields -->
+    
+
+          <div class="row">
+            <label>Customer's Name</label>
+            <input class="form-control" placeholder="e.g. Juan dela Cruz">
+
+            <label>Processor's Name</label>
+            <input class="form-control" placeholder="e.g. Pro dela Cruz">
+
+            <label>Processor's ID Number</label>
+            <input class="form-control" placeholder="e.g. 2012-09876">
+
+            <label>Processor's Department</label>
+            <input class="form-control" placeholder="e.g. Records">
+
+            <label>Transaction Type</label>
+            <select class="form-control">
+              <option>Type 1</option>
+              <option>Type 2</option>
+              <option>Type 3</option>
+              <option>Type 4</option>
+              <option>Type 5</option>
+            </select>
+
+            <label>Transaction Stage</label>
+            <select class="form-control">
+              <option>Stage 1</option>
+              <option>Stage 2</option>
+              <option>Stage 3</option>
+              <option>Stage 4</option>
+              <option>Stage 5</option>
+            </select>
+        </div>
+
+<!-- /Form input fields-->
