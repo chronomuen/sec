e4b833583b67b2a704f843b708aed780005bf557
roannadavid <roannadavid@yahoo.com>
2015-07-01 13:56:36 +0800

    Added create transaction page for users

---
+++ resources/views/admin/_sidebar.blade.php
@@ -0,0 +1,15 @@
+@section('user_info')
+<!-- user info -->
+<p>First Lastname</p>
+<!-- job title -->
+<a href="#"><i class="fa fa-circle text-success"></i>Administrator</a>
+@stop
+
+@section('sidebar')
+  <li>
+      <a href="admin_index"><i class="fa fa-fw fa-reorder"></i>View Transactions</a>
+  </li>
+  <li>
+      <a href="admin_new_transaction"><i class="fa fa-fw fa-plus-square"></i>Create Transaction</a>
+  </li>
+@stop
---
+++ resources/views/processor/_sidebar.blade.php
@@ -0,0 +1,15 @@
+@section('user_info')
+<!-- user info -->
+<p>First Lastname</p>
+<!-- job title -->
+<a href="#"><i class="fa fa-circle text-success"></i>Processor</a>
+@stop
+
+@section('sidebar')
+  <li>
+      <a href="processor_index"><i class="fa fa-fw fa-reorder"></i>View Transactions</a>
+  </li>
+  <li>
+      <a href="processor_new_transaction"><i class="fa fa-fw fa-plus-square"></i>Create Transaction</a>
+  </li>
+@stop
