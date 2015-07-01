49d609410f8b8ec78fd64922892d1c6fc64fd1b1
roannadavid <roannadavid@yahoo.com>
2015-07-01 15:23:09 +0800

    Working on new user page

--- resources/views/superadmin/new_user.blade.php
+++ resources/views/superadmin/new_user.blade.php
@@ -10,18 +10,24 @@ Create New User
 <script src="/js/superadmin.js" type="text/javascript"></script>
 @stop
 
-
 @section('content')
-<div class="row">
-  <div class="col-lg-12">
-        <div class="form-group">
-          @include('forms/_new_user')
-          <div class="row" >
-            <br/>
-            <a href="superadmin_success"><button type="submit" class="btn btn-default"> Create User </button></a>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-11">
+      <div class="panel">
+        <div class="panel-body">
+          <div class="col-lg-6">
+            <div class="form-group">
+              @include('forms/_new_user')
+              <div class="row" >
+                <br/>
+                <a href="superadmin_success"><button type="submit" class="btn btn-default"> Create User </button></a>
+              </div>
+            </div>
           </div>
         </div>
-      
+      </div>
+    </div>
   </div>
 </div>
 
