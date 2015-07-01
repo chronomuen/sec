806e8f0f2f6595db4566ec9205c4a61fd061c318
roannadavid <roannadavid@yahoo.com>
2015-07-01 13:09:15 +0800

    Created transaction creation page

---
+++ resources/views/superadmin/_sidebar.blade.php
@@ -0,0 +1,30 @@
+@section('user_info')
+<!-- user info -->
+<p>First Lastname</p>
+<!-- job title -->
+<a href="#"><i class="fa fa-circle text-success"></i>Super <br/>Administrator</a>
+@stop
+
+@section('sidebar')
+  <li>
+      <a href="superadmin_index"><i class="fa fa-fw fa-reorder"></i>View Transactions</a>
+  </li>
+  <li>
+      <a href="superadmin_new_transaction"><i class="fa fa-fw fa-plus-square"></i>Create Transaction</a>
+  </li>
+  <li>
+      <a href="#"><i class="fa fa-fw fa-exchange"></i>Assign Transactions</a>
+  </li>
+  <li class="treeview">
+    <a href="#">
+      <i class="fa fa-group"></i>
+      <span>Manage Users</span>
+      <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+      <li><a href="#"><i class="fa fa-list"></i> View Users</a></li>
+      <li><a href="#"><i class="fa fa-user"></i> Set Administrators</a></li>
+      <li><a href="#"><i class="fa fa-plus-circle"></i> Add User</a></li>
+    </ul>
+  </li>
+@stop
--- resources/views/superadmin/index.blade.php
+++ resources/views/superadmin/index.blade.php
@@ -1,40 +1,10 @@
 @extends('default')
+<!-- User Sidebar -->
+@include('superadmin/_sidebar')
 
 @section('header')
 Transaction List
 @stop
-
-@section('user_info')
-<!-- user info -->
-<p>First Lastname</p>
-<!-- job title -->
-<a href="#"><i class="fa fa-circle text-success"></i>Super <br/>Administrator</a>
-@stop
-
-@section('sidebar')
-  <li class="active">
-      <a href="processor_index"><i class="fa fa-fw fa-reorder"></i>View Transactions</a>
-  </li>
-  <li>
-      <a href="#"><i class="fa fa-fw fa-plus-square"></i>Create Transaction</a>
-  </li>
-  <li>
-      <a href="#"><i class="fa fa-fw fa-exchange"></i>Assign Transactions</a>
-  </li>
-  <li class="treeview">
-    <a href="#">
-      <i class="fa fa-group"></i>
-      <span>Manage Users</span>
-      <i class="fa fa-angle-left pull-right"></i>
-    </a>
-    <ul class="treeview-menu">
-      <li><a href="#"><i class="fa fa-list"></i> View Users</a></li>
-      <li><a href="#"><i class="fa fa-user"></i> Set Administrators</a></li>
-      <li><a href="#"><i class="fa fa-plus-circle"></i> Add User</a></li>
-    </ul>
-  </li>
-@stop
-
 
 @section('content')
 <div class="panel panel-primary">
