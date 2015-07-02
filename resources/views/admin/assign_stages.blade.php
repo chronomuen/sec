f8bd698a0992b05a1cada8d622854aa2d8247bfd
roannadavid <roannadavid@yahoo.com>
2015-07-02 12:19:57 +0800

    commit

--- resources/views/admin/assign_stages.blade.php
+++ resources/views/admin/assign_stages.blade.php
@@ -27,98 +27,89 @@
 
 <div class="container">
 	<div class="row">
-        
 		<div class="col-md-9">
-    	 <table class="table table-list-search">
-                    <thead>
-                        <tr>
-                            <th><center>Processor</center></th>
-                            <th><center>Department</center></th>
-                            <th><center>Stages</center></th>
-                            <th><center>Search Stage</center></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Z, Jay</td>
-                            <td>Rap Department</td>
-                            <td>
-								<select id="select" class="form-control">
-									<option disabled selected> -- show stages -- </option>
-									<option>stage1</option>
-									<option>stage2</option>
-									<option>stage3</option>
-									<option>stage4</option>
-									<option>stage5</option>
-								</select>
-							</td>
-                            <td>
+    	<table class="table table-list-search">
+        <thead>
+          <tr>
+	          <th><center>Processor</center></th>
+	          <th><center>Department</center></th>
+	          <th><center>Stages</center></th>
+	          <th><center>Search Stage</center></th>
+          </tr>
+        </thead>
+        <tbody>
+	        <tr>
+            <td>Z, Jay</td>
+            <td>Rap Department</td>
+            <td>
+							<select id="select" class="form-control">
+								<option disabled selected> -- show stages -- </option>
+								<option>stage1</option>
+								<option>stage2</option>
+								<option>stage3</option>
+								<option>stage4</option>
+								<option>stage5</option>
+							</select>
+						</td>
+            <td>
+							<div style="width:150px">
+								<span class="input-group-btn">
+									<button type="submit" class="btn btn-default inline"><i class="glyphicon glyphicon-search"></i></button>
+								</span>
+								<input class="form-control inline" id="system-search" name="q" placeholder="Search for" >
+
+							</div>
+						</td>
+          </tr>
+          <tr>
+            <td>Gaga, Lady</td>
+            <td>Pop Department</td>
+            <td>
+							<select id="select" class="form-control">
+								<option disabled selected> -- show stages -- </option>
+								<option>stage1</option>
+								<option>stage2</option>
+								<option>stage3</option>
+								<option>stage4</option>
+								<option>stage5</option>
+							</select>
+						</td>
+            <td>
+							<span>
 								<div style="width:150px">
 									<input class="form-control" id="system-add" name="q" placeholder="Add Stage">
-									<span class="input-group-btn">
-										<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-plus"></i></button>
-									</span>
 								</div>
-							</td>
-                        </tr>
-                        <tr>
-                            <td>Gaga, Lady</td>
-                            <td>Pop Department</td>
-                            <td>
-								<select id="select" class="form-control">
-									<option disabled selected> -- show stages -- </option>
-									<option>stage1</option>
-									<option>stage2</option>
-									<option>stage3</option>
-									<option>stage4</option>
-									<option>stage5</option>
-								</select>
-							</td>
-                            <td>
-								<span>	
-									<div style="width:150px">
-										<input class="form-control" id="system-add" name="q" placeholder="Add Stage">
-									</div>
-								</span>
-								<span class="input-group-btn" style="float:right">
-									<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-plus"></i></button>
-								</span>
-								
-							</td>
-							
-                        </tr>
-                        <tr>
-                            <td>9, Gloc</td>
-                            <td>OPM Department</td>
-                            <td>
-								<select id="select" class="form-control">
-									<option disabled selected> -- show stages -- </option>
-									<option>stage1</option>
-									<option>stage2</option>
-									<option>stage3</option>
-									<option>stage4</option>
-									<option>stage5</option>
-								</select>
-							</td>
-                            
-							<td>
-								<div style="width:150px">
-								
-									<input class="form-control" id="system-add" name="q" placeholder="Add Stage">
-								
-									
-									<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-plus"></i></button>
-									
-								
-								</div>
-								
-							</td>
-                        </tr>
-                    </tbody>
-                </table>   
+							</span>
+							<span class="input-group-btn" style="float:right">
+								<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-plus"></i></button>
+							</span>
+						</td>
+					</tr>
+          <tr>
+            <td>9, Gloc</td>
+            <td>OPM Department</td>
+            <td>
+							<select id="select" class="form-control">
+								<option disabled selected> -- show stages -- </option>
+								<option>stage1</option>
+								<option>stage2</option>
+								<option>stage3</option>
+								<option>stage4</option>
+								<option>stage5</option>
+							</select>
+						</td>
+						<td>
+							<div style="width:150px">
+								<input class="form-control" id="system-add" name="q" placeholder="Add Stage">
+								<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-plus"></i></button>
+							</div>
+						</td>
+          </tr>
+      	</tbody>
+	    </table>
 		</div>
 	</div>
 </div>
 
- 
+
 @stop
