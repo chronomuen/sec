01f9e80a533fd248cecd0775bc4af25d01854152
roannadavid <roannadavid@yahoo.com>
2015-07-02 21:11:56 +0800

    changes to superadmin index

--- resources/views/superadmin/index.blade.php
+++ resources/views/superadmin/index.blade.php
@@ -32,8 +32,9 @@
 				<tbody>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item" data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID1">
 							<b>Transaction1</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -42,8 +43,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID2">
 							<b>Transaction2</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -52,8 +54,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID3">
 							<b>Transaction3</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -62,8 +65,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID4">
 							<b>Transaction4</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -72,8 +76,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID5">
 							<b>Transaction5</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -82,8 +87,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID6">
 							<b>Transaction6</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -92,8 +98,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID7">
 							<b>Transaction7</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -102,8 +109,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID8">
 							<b>Transaction8</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -112,8 +120,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-target="#transactionModal" data-transaction-id="transactionID9">
 							<b>Transaction9</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -122,8 +131,9 @@
 					</tr>
 					<tr>
 						<td>
-						<a href="#" class="list-group-item" data-toggle="modal" data-target="#openModal">
+						<a href="#" class="list-group-item " data-toggle="modal" data-transaction-id="transactionID10" data-target="#transactionModal">
 							<b>Transaction10</b><br/>
+							Transaction ID<br/>
 							Customer<br/>
 							Transaction Type<br/>
 							Transaction Stage
@@ -137,41 +147,48 @@
 </div>
 
 <!-- Modal -->
-<div id="openModal" class="modal fade" role="dialog">
+<div id="transactionModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
+        <h4 class="modal-title">
+					Process Transaction
+					<b><p id="transaction_title"></p></b>
+				</h4>
       </div>
       <div class="modal-body">
+				<label>Send transaction to: </label>
         <div class="form-group">
-			<select id="select" class="form-control">
-				<option disabled selected> -- select department -- </option>
-				<option>department1</option>
-				<option>department2</option>
-				<option>department3</option>
-				<option>department4</option>
-				<option>department5</option>
-			</select>
-		</div>
-		<p>
-		Proceed to
-		<b>
-		<class id="department">
+					<select id="select" class="form-control">
+						<option disabled selected> -- select department -- </option>
+						<option>department1</option>
+						<option>department2</option>
+						<option>department3</option>
+						<option>department4</option>
+						<option>department5</option>
+					</select>
+				</div>
+				<p>
+				Proceed to
+				<b>
+				<class id="department">
 
-		</class>
-		</b>
-		at room something something database.
-		</p>
-
-
-
+				</class>
+				</b>
+				at room something something database.
+				</p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				{!! Form::open(array('url' => '/transaction/process')) !!}
+				  {!! Form::hidden('transactionID', 'value', array('id' => 'transaction_id')) !!}
+					{!! Form::hidden('deptName', 'value', array('id' => 'deptName')) !!}
+				{!! Form::submit('Submit', ['class' => 'btn btn-default']) !!}
+
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				{!! Form::close() !!}
       </div>
     </div>
 
