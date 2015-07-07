a611e13de85ae43524d2ded1a5c545ef4801b2e3
roannadavid <roannadavid@yahoo.com>
2015-07-07 10:51:56 +0800

    Changed user sidebars

--- resources/views/processor/_sidebar.blade.php
+++ resources/views/processor/_sidebar.blade.php
@@ -1,15 +1,24 @@
 @section('user_info')
 <!-- user info -->
-<p>First Lastname</p>
+<p>Julian Felipe</p>
 <!-- job title -->
 <a href="/processor"><i class="fa fa-circle text-success"></i>Processor</a>
 @stop
 
 @section('sidebar')
-  <li>
-      <a href="/processor"><i class="fa fa-fw fa-reorder"></i>View Transactions</a>
-  </li>
-  <li>
-      <a href="/processor/create_transaction"><i class="fa fa-fw fa-plus-square"></i>Create Transaction</a>
-  </li>
+<li>
+    <a href="/processor"><i class="fa fa-fw fa-search"></i>Search Transaction</a>
+</li>
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-spinner"></i>
+    <span>Process</span>
+    <i class="fa fa-angle-left pull-right"></i>
+  </a>
+  <ul class="treeview-menu">
+    <li><a href="/processor/view_users"><i class="fa fa-file"></i>Create Transaction</a></li>
+    <li><a href="/processor/create_user"><i class="fa fa-rotate-right"></i>Update Transaction</a></li>
+    <li><a href="/processor/create_user"><i class="fa fa-send"></i>Out</a></li>
+  </ul>
+</li>
 @stop
