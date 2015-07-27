494db1a52c9997901a5af0dab8dc61b60cc91b8a
bower-chosen <otsukakenneth@yahoo.com>
2015-07-27 08:15:05 +0800

    commit

--- resources/views/superadmin/out_transaction.blade.php
+++ resources/views/superadmin/out_transaction.blade.php
@@ -13,20 +13,20 @@ Logout Transaction
 @stop
 
 @section('content')
-@if(!empty($transaction))
-	{!! Form::model($transaction, ['method' => 'PATCH', 'route' =>  ['transaction.update', $transaction->transaction_id] ]) !!}
-	{!! View::make('forms/_out_transaction', array('transaction' => $transaction, 'logs' => $logs, 'recentLog' => $recentLog, 'users' => $users, 'flag' => $flag, 'remarks' => $remarks))  !!}
-@else
-	<div style="padding-right:20px; padding-left:20px">
-		<div class="row">
-			<div class="panel">
-				<div class="panel-body">
-					<div class="alert alert-danger">
-						Transaction not found!
+	@if(!empty($transaction))
+		{!! Form::model($transaction, ['method' => 'PATCH', 'route' =>  ['transaction.update', $transaction->transaction_id] ]) !!}
+		{!! View::make('forms/_out_transaction', array('transaction' => $transaction, 'logs' => $logs, 'recentLog' => $recentLog, 'users' => $users, 'flag' => $flag, 'remarks' => $remarks)) !!}
+	@else
+		<div style="padding-right:20px; padding-left:20px">
+			<div class="row">
+				<div class="panel">
+					<div class="panel-body">
+						<div class="alert alert-danger">
+							Transaction not found!
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-@endif
+	@endif
 @stop
